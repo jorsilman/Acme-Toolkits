11b0dec0abfc3a163ed320776066f7d6f0ac7fae
jorsilman <jorsilman@alum.us.es>
2022-03-15 18:56:59 +0100

    Task-035: 1/2

---
+++ src/main/java/acme/entities/toolkit/Toolkit.java
@@ -0,0 +1,46 @@
+package acme.entities.toolkit;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+public class Toolkit extends AbstractEntity{
+	
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Column(unique=true)
+	@Pattern(regexp="^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
+	protected String			code;
+
+	@NotBlank
+	@Length(max=100)
+	protected String			title;
+	
+	@NotBlank
+	@Length(max=255)
+	protected String			description;
+	
+	@NotBlank
+	@Length(max=255)
+	protected String			assemblyNotes;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
