999c733a794c86f9387eada85387800009019e71
jorsilman <jorsilman@alum.us.es>
2022-03-16 19:31:58 +0100

    Task-034:Done

--- src/main/java/acme/entities/tool/Tool.java
+++ src/main/java/acme/entities/tool/Tool.java
@@ -2,13 +2,17 @@ package acme.entities.tool;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+import acme.entities.toolkit.Toolkit;
 import acme.framework.datatypes.Money;
 import acme.framework.entities.AbstractEntity;
 import lombok.Getter;
@@ -32,6 +36,7 @@ public class Tool extends AbstractEntity {
 	protected String			name;
 
 	@Column(unique=true)
+	@NotBlank
 	@Pattern(regexp="^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
 	protected String			code;
 	
@@ -44,6 +49,7 @@ public class Tool extends AbstractEntity {
 	protected String			description;
 	
 	@Min(0)
+	@NotNull
 	protected Money				retailPrice;
 	
 	@URL
@@ -52,5 +58,8 @@ public class Tool extends AbstractEntity {
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
-
+	@NotNull
+	@Valid
+	@ManyToOne(optional=true)
+	protected Toolkit			toolkit;
 }
