a9fe8952237c9eed5b128b6fcdeb3e9c3582b486
RingoML <josemanuelmartinluque@outlook.com>
2022-03-18 18:20:36 +0100

    Task 039: Fix
    Fix alignment

--- src/main/java/acme/forms/AdministratorDashboard.java
+++ src/main/java/acme/forms/AdministratorDashboard.java
@@ -14,29 +14,29 @@ public class AdministratorDashboard implements Serializable{
 
 		// Attributes -------------------------------------------------------------
 
-		int												totalNumberOfComponents;
-		Map<Pair<String, String>, Double>				averageRetailPriceOfComponentsByTechnologyAndCurrency;
-		Map<Pair<String, String>, Double>				deviationRetailPriceOfComponentsByTechnologyAndCurrency;
-		Map<Pair<String, String>, Double>				minimumRetailPriceOfComponentsByTechnologyAndCurrency;
-		Map<Pair<String, String>, Double>				maximumRetailPriceOfComponentsByTechnologyAndCurrency;
+		int                                             totalNumberOfComponents;
+		Map<Pair<String, String>, Double>               averageRetailPriceOfComponentsByTechnologyAndCurrency;
+		Map<Pair<String, String>, Double>               deviationRetailPriceOfComponentsByTechnologyAndCurrency;
+		Map<Pair<String, String>, Double>               minimumRetailPriceOfComponentsByTechnologyAndCurrency;
+		Map<Pair<String, String>, Double>               maximumRetailPriceOfComponentsByTechnologyAndCurrency;
 		// Map<Pair<Technology, Currency>, Amount>
 		
 		
-		int												totalNumberOfTools;
-		Map<String, Double>								averageRetailPriceOfToolsByCurrency;
-		Map<String, Double>								deviationRetailPriceOfToolsByCurrency;
-		Map<String, Double>								minimumRetailPriceOfToolsByCurrency;
-		Map<String, Double>								maximumRetailPriceOfToolsByCurrency;
+		int                                             totalNumberOfTools;
+		Map<String, Double>                             averageRetailPriceOfToolsByCurrency;
+		Map<String, Double>                             deviationRetailPriceOfToolsByCurrency;
+		Map<String, Double>                             minimumRetailPriceOfToolsByCurrency;
+		Map<String, Double>                             maximumRetailPriceOfToolsByCurrency;
 		// Map<Currency, Amount>
 		
 		
-		int												totalNumberOfProposedPatronages;
-		int												totalNumberOfAcceptedPatronages;
-		int												totalNumberOfDeniedPatronages;
-		Map<PatronageStatus, Double>					averageBudgetOfPatronagesByStatus;
-		Map<PatronageStatus, Double>					deviationBudgetOfPatronagesByStatus;
-		Map<PatronageStatus, Double>					minimumBudgetOfPatronagesByStatus;
-		Map<PatronageStatus, Double>					maximumBudgetOfPatronagesByStatus;
+		int                                             totalNumberOfProposedPatronages;
+		int                                             totalNumberOfAcceptedPatronages;
+		int                                             totalNumberOfDeniedPatronages;
+		Map<PatronageStatus, Double>                    averageBudgetOfPatronagesByStatus;
+		Map<PatronageStatus, Double>                    deviationBudgetOfPatronagesByStatus;
+		Map<PatronageStatus, Double>                    minimumBudgetOfPatronagesByStatus;
+		Map<PatronageStatus, Double>                    maximumBudgetOfPatronagesByStatus;
 		// Map<Status, Amount>
 		
 		
