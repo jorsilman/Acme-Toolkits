528295b2525bc71543b9e45c4f4badeffdff1191
frareymad <frareymad@alum.us.es>
2022-04-22 11:23:34 +0200

    TASK 053: DONE

--- src/main/java/acme/features/authenticated/inventor/tool/ToolRepository.java
+++ src/main/java/acme/features/authenticated/inventor/tool/ToolRepository.java
@@ -11,6 +11,8 @@ import acme.framework.repositories.AbstractRepository;
 @Repository
 public interface ToolRepository extends AbstractRepository{
 	
+	//Cojo el id del inventor de un cierto item i y lo comparo con el id del inventor actualmente logueado y que dicho item sea de tipo TOOL
+	
 	@Query("SELECT i FROM Item i WHERE i.inventor.id = :id AND i.itemType = acme.entities.item.ItemType.TOOL")
 	Collection<Item> findToolsByInventorId(int id);
 	
