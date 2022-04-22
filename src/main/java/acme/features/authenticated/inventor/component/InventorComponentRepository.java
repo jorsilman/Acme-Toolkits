312d011a626c4f5350ea629a16835adaf38252fa
frareymad <frareymad@alum.us.es>
2022-04-22 16:57:09 +0200

    TASK 052 : DONE DEF

--- src/main/java/acme/features/authenticated/inventor/component/InventorComponentRepository.java
+++ src/main/java/acme/features/authenticated/inventor/component/InventorComponentRepository.java
@@ -11,6 +11,8 @@ import acme.framework.repositories.AbstractRepository;
 @Repository
 public interface InventorComponentRepository extends AbstractRepository{
 	
+	//Dado el id de un determinado inventor de un item lo comparo con el id del usuario logueado y veo que ese item sea de tipo COMPONENT
+	
 	@Query("SELECT i FROM Item i WHERE i.inventor.id = :id AND i.itemType = acme.entities.item.ItemType.COMPONENT")
 	Collection<Item> findComponentsByInventorId(int id);
 	
