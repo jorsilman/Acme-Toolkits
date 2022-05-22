a66b6d1ab22b93375b89328938725853feb39d34
jorsilman <jorsilman@alum.us.es>
2022-05-22 18:55:41 +0200

    Creado servicio y repositorio

--- src/main/java/acme/features/spam/SpamDetector.java
+++ src/main/java/acme/features/spam/SpamDetector.java
@@ -1,6 +1,26 @@
 package acme.features.spam;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SpamDetector {
 
+	@Autowired
+	private SystemConfigurationRepository repository;
+	
+	public List<String> normalizarTexto(final String texto){
+		return null;
+	}
+	
+	public boolean isSpam(final String texto) {
+		final boolean res = true;
+		final List<String> textoNormalizado = this.normalizarTexto(texto);
+		
+		final List<String> palabrasWeak = this.repository.getWeak();
+		final List<String> palabrasStrong = this.repository.getStrong();
+		return res;
+	}
 }
---
+++ src/main/java/acme/features/spam/SystemConfigurationRepository.java
@@ -0,0 +1,12 @@
+package acme.features.spam;
+
+import org.springframework.stereotype.Repository;
+
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface SystemConfigurationRepository extends AbstractRepository{
+	
+	
+
+}
